c6d11a1779e654e8db69a11bbb25a555d03be2d7
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2026-05-11 12:34:58 +0530

    Update MEM_TOP.sv

--- RAM_VERIFICATION/MEM_TOP.sv
+++ RAM_VERIFICATION/MEM_TOP.sv
@@ -4,7 +4,6 @@ logic rst;
 intf h_intf(.clk(clk),.rst(rst));//interface
 test tb();
 mem dut(.clk(h_intf.clk),.rst(h_intf.rst),.addr(h_intf.addr),.wr_rd(h_intf.wr_rd),.wr_data(h_intf.wr_data),.rd_data(h_intf.rd_data));
-
 initial begin
 clk=0;
 forever #5 clk=~clk;
@@ -15,16 +14,11 @@ cfg::vintf=h_intf;
 end
 
 initial begin
-#200000;$display("Coverage =%0.2f%%",h_intf.cg.get_inst_coverage());
-$finish;
-end
-
-initial begin
 $display("////////////////////TOP MODULE/////////////////////");
-rst=1;
+rst=0;
 repeat(2)
 @(posedge clk);
-rst=0;
+rst=1;
 end
 
 endmodule
