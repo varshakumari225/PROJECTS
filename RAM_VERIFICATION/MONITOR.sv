c1b8be65b8b5036868b8227dd8e05c0bdbe5385a
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2026-05-11 12:32:42 +0530

    Create MONITOR.sv

---
+++ RAM_VERIFICATION/MONITOR.sv
@@ -0,0 +1,24 @@
+class monitor;
+transaction tx;
+virtual intf m_intf;
+
+task run();
+$display("//////////MONITOR//////////");
+m_intf=cfg::vintf;
+wait(m_intf.rst==1);
+forever begin
+@(m_intf.cb);
+tx=new();
+tx.addr=m_intf.cb.addr;
+tx.wr_rd=m_intf.cb.wr_rd;
+tx.wr_data=m_intf.cb.wr_data;
+if(m_intf.wr_rd==0)begin
+@(m_intf.cb);
+@(m_intf.cb);
+tx.rd_data=m_intf.cb.rd_data;
+end
+cfg::mon2cov.put(tx);
+end
+endtask
+
+endclass
