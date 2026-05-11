de6bec79dcf75f08eb713ec74ddd8e82b6cfff1d
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2026-05-11 12:32:12 +0530

    Update TEST.sv

--- RAM_VERIFICATION/TEST.sv
+++ RAM_VERIFICATION/TEST.sv
@@ -1,11 +1,14 @@
 program test();
 env e;
+
 initial begin
 $display("");
 $display("////////////Test////////////////");
 $display("");
-
 e=new();
 e.run();
+#15000;
+$display("Simulation end at [%0t]",$time);
 end
+
 endprogram
